3c9814886288199601112e61ce8f425cc64a5e82
bellamkaddem salma <salmabellamkaddem11@gmail.com>
2024-01-20 23:27:46 +0100

    first commit

---
+++ Migrations/20240114083436_FirstModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FineBlog.Migrations
+{
+    public partial class FirstModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
